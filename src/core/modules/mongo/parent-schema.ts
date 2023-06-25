aa4bb2a270227aa7c87c8259c083ffd2374fa021
sixbeeshades <abhijith.p.subash@gmail.com>
2023-06-25 17:23:34 +0530

    fix

--- src/core/modules/mongo/parent-schema.ts
+++ src/core/modules/mongo/parent-schema.ts
@@ -3,27 +3,27 @@ import { prop, getModelForClass } from "@typegoose/typegoose";
 interface IParentSchema {
   active: boolean;
   created_at: Date;
-  created_by: string;
+  created_by?: string;
   updated_at: Date;
-  updated_by: string;
+  updated_by?: string;
 }
 
 export class ParentSchema implements IParentSchema {
   //   @prop({ type: String, required: true })
   //   public _id!: string;
 
-  @prop({ type: Boolean })
+  @prop({ type: Boolean, required: true, default: true })
   public active!: boolean;
 
   @prop({ type: Date, required: true, default: Date.now })
   public created_at!: Date;
 
-  @prop({ type: String, required: true })
-  public created_by!: string;
+  // @prop({ type: String, required: true })
+  // public created_by!: string;
 
   @prop({ type: Date, required: true, default: Date.now })
   public updated_at!: Date;
 
-  @prop({ type: String, required: true })
-  public updated_by!: string;
+  // @prop({ type: String, required: true })
+  // public updated_by!: string;
 }
