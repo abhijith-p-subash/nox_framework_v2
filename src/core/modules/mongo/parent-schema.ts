dea8e0becf28e76f8b8e20b60accf0f52baa327d
sixbeeshades <abhijith.p.subash@gmail.com>
2023-06-25 16:06:22 +0530

    parent schema fix

--- src/core/modules/mongo/parent-schema.ts
+++ src/core/modules/mongo/parent-schema.ts
@@ -7,17 +7,15 @@ export class ParentSchema {
   @prop({ type: Boolean })
   public active!: Boolean;
 
-  @prop({ type: Date, required: true })
+  @prop({ type: Date, required: true, default: Date.now })
   public created_at!: Date;
 
   @prop({ type: String, required: true })
   public created_by!: string;
 
-  @prop({ type: Date, required: true })
+  @prop({ type: Date, required: true, default: Date.now })
   public updated_at!: Date;
 
   @prop({ type: String, required: true })
   public updated_by!: string;
 }
-
-export const UserModel = getModelForClass(ParentSchema);
