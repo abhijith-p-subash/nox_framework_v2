64b2f5fdb6d6580c07be6b98db9a79bdf56dd6f7
sixbeeshades <abhijith.p.subash@gmail.com>
2023-06-25 20:35:45 +0530

    email service

---
+++ src/core/modules/email/email.service.ts
@@ -0,0 +1,37 @@
+import nodemailer from "nodemailer";
+import { Job } from "src/core/utils/job";
+import dotev from "dotenv";
+
+export class EmailService {
+  transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_USERNAME,
+    port: 25,
+    service: process.env.EMAIL_SERVICE,
+    secure: false,
+    auth: {
+      // user: "abhijithpsubash110@gmail.com",
+      // pass: "rahwaqxxoxnyfmkg"
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  async sendEmail(job: Job) {
+    const mailData = job.body;
+    const mailOPtions = {
+      from: process.env.EMAIL_USERNAME,
+      to: mailData?.email,
+      subject: mailData?.subject,
+      text: mailData?.mailBody,
+    };
+
+    try {
+      const info = await this.transporter.sendMail(mailOPtions);
+      console.log(info);
+      return info;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
